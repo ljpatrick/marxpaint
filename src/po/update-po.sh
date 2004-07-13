17e41d0f98561908acff482e04aa018dea66a259
Karl Ove Hufthammer <huftis@users.sourceforge.net>
2004-07-13 13:26:04 +0000

    Removed line numbers to avoid unnecessary CVS updates.

--- src/po/update-po.sh
+++ src/po/update-po.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 intltool-update --pot
+msguniq --no-location tuxpaint.pot > temp.tmp && mv -f temp.tmp tuxpaint.pot
 for i in *.po ; do
-  msgmerge $i tuxpaint.pot > tmp.po && cat tmp.po > $i
+  msgmerge $i tuxpaint.pot > temp.tmp && mv -f temp.tmp $i
 done
 cd ..
 intltool-merge -d -u po tuxpaint.desktop.in tuxpaint.desktop
