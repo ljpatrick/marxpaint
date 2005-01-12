93b385111fbe98f80625d57cd22378910ce6d406
Karl Ove Hufthammer <huftis@users.sourceforge.net>
2005-01-12 19:53:10 +0000

    New, (hopefully) improved and beautiful colour palette.

--- src/colors.h
+++ src/colors.h
@@ -17,18 +17,17 @@
 
 enum {
   COLOR_BLACK,
-  COLOR_DARKGREY,
-  COLOR_LIGHTGRAY,
+  COLOR_GRAY,
   COLOR_WHITE,
   COLOR_RED,
   COLOR_ORANGE,
   COLOR_YELLOW,
-  COLOR_NEON,
-  COLOR_GREEN,
+  COLOR_LIGHTGREEN,
+  COLOR_DARKGREEN,
   COLOR_SKYBLUE,
   COLOR_BLUE,
+  COLOR_VIOLET,
   COLOR_PURPLE,
-  COLOR_MAGENTA,
   COLOR_PINK,
   COLOR_BROWN,
   COLOR_TAN,
@@ -41,20 +40,19 @@ enum {
 
 const int color_hexes[NUM_COLORS][3] = {
   {0, 0, 0},       /* Black */
-  {128, 128, 128}, /* Dark Gray */
-  {192, 192, 192}, /* Light Grey */
+  {162, 162, 162}, /* Gray */
   {255, 255, 255}, /* White */
   {255,   0,   0}, /* Red */
-  {255, 128,   0}, /* Orange */
-  {255, 255,   0}, /* Yellow */
-  {140, 255,   0}, /* Neon */
-  { 33, 148,  33}, /* Green */
-  {138, 168, 205}, /* Sky blue */
-  {  0,   0, 255}, /* Blue */
-  {128,   0, 128}, /* Purple */
-  {255,   0, 255}, /* Magenta */
+  {255, 162,  64}, /* Orange */
+  {255, 255, 128}, /* Yellow */
+  {160, 228, 128}, /* Light green */
+  { 33, 148,  70}, /* Dark green */
+  {156, 200, 245}, /* Light blue */
+  { 85, 130, 255}, /* Blue */
+  {186, 157, 255}, /* Violet */
+  {150,   0, 150}, /* Purple */
   {255, 165, 211}, /* Pink */
-  {128,  96,   0}, /* Brown */
+  {128,  80,   0}, /* Brown */
   {226, 189, 166}, /* Tan */
   {247, 228, 219}  /* Beige */
 };
@@ -64,18 +62,17 @@ const int color_hexes[NUM_COLORS][3] = {
 
 const char * const color_names[NUM_COLORS] = {
   gettext_noop("Black!"),
-  gettext_noop("Dark Gray!"), // using both valid spellings
-  gettext_noop("Light Grey!"), // using both valid spellings
+  gettext_noop("Gray!"),
   gettext_noop("White!"),
   gettext_noop("Red!"),
   gettext_noop("Orange!"),
   gettext_noop("Yellow!"),
-  gettext_noop("Neon green!"),
-  gettext_noop("Green!"),
-  gettext_noop("Sky blue!"),
+  gettext_noop("Light green!"),
+  gettext_noop("Dark green!"),
+  gettext_noop("Light blue!"),
   gettext_noop("Blue!"),
+  gettext_noop("Violet!"),
   gettext_noop("Purple!"),
-  gettext_noop("Magenta!"),
   gettext_noop("Pink!"),
   gettext_noop("Brown!"),
   gettext_noop("Tan!"),
