6c953ea896080b58cbce1df222fef9e092af1938
Karl Ove Hufthammer <huftis@users.sourceforge.net>
2008-09-21 15:03:52 +0000

    Use better msgmerge options for updating PO files.

--- src/po/update-po.sh
+++ src/po/update-po.sh
@@ -4,7 +4,7 @@ intltool-update --pot
 msguniq tuxpaint.pot > temp.tmp && mv -f temp.tmp tuxpaint.pot
 for i in *.po ; do
   echo $i
-  msgmerge $i tuxpaint.pot > temp.tmp && mv -f temp.tmp $i
+  msgmerge --update --previous $i tuxpaint.pot
 done
 cd ..
 intltool-merge -d -u po tuxpaint.desktop.in tuxpaint.desktop
