c7a4f37a53dfb82089613f273da61e0f09d8f8ee
Pere Pujal i Carabantes <perepujal@users.sourceforge.net>
2011-05-14 00:01:32 +0000

    Ordering the  magic tools by localized translation.

--- src/tuxpaint.c
+++ src/tuxpaint.c
@@ -17681,7 +17681,7 @@ static int magic_sort(const void * a, const void * b)
   magic_t * am = (magic_t *) a;
   magic_t * bm = (magic_t *) b;
 
-  return(strcmp(am->name, bm->name));
+  return(strcmp(gettext(am->name), gettext(bm->name)));
 }
 
 
