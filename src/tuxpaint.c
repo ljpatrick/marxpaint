330ab1960085d1f4b7d32a2f839521685be8dab0
William Kendrick <wkendrick@users.sourceforge.net>
2014-04-16 07:02:07 +0000

    Updated copyright date on title screen.

--- src/tuxpaint.c
+++ src/tuxpaint.c
@@ -22,7 +22,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   (See COPYING.txt)
   
-  June 14, 2002 - April 15, 2014
+  June 14, 2002 - April 16, 2014
 */
 
 
@@ -22894,7 +22894,7 @@ static void setup(void)
   printf("%s\n", tmp_str);
 #endif
 
-  snprintf(tmp_str, sizeof(tmp_str), "© 2002–2012 Bill Kendrick et al.");
+  snprintf(tmp_str, sizeof(tmp_str), "© 2002–2014 Bill Kendrick et al.");
   tmp_surf = render_text(medium_font, tmp_str, black);
   dest.x = 10;
   dest.y = WINDOW_HEIGHT - img_progress->h - (tmp_surf->h * 2);
