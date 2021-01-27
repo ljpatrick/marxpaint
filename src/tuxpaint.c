3ebb76e7bb8861fb2f93dad993bddd3445708d50
Bill Kendrick <nbs@sonic.net>
2021-01-26 23:10:05 -0800

    Mend backward logic in font picking for buttons
    Was choosing largest font (and scaling down) for medium-sized
    buttons, and medium font for large-sized buttons.

--- src/tuxpaint.c
+++ src/tuxpaint.c
@@ -22,7 +22,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   (See COPYING.txt)
 
-  June 14, 2002 - January 24, 2021
+  June 14, 2002 - January 26, 2021
 */
 
 
@@ -7948,7 +7948,7 @@ static SDL_Surface *do_render_button_label(const char *const label)
 
   if (button_w <= ORIGINAL_BUTTON_SIZE)
     myfont = small_font;
-  else if (button_w >= ORIGINAL_BUTTON_SIZE * 2)
+  else if (button_w <= ORIGINAL_BUTTON_SIZE * 2)
     myfont = medium_font;
   else
     myfont = large_font;
